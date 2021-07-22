2259faf529f4493309e6932d6b25bd861325178a
T.Hori <beat3526@gmail.com>
2021-07-23 04:42:03 +0900

    PlayFramework ユーザ登録・編集画面の実装

--- play-scala-seed/app/controllers/UserController.scala
+++ play-scala-seed/app/controllers/UserController.scala
@@ -1,13 +1,16 @@
 package controllers
 
+import controllers.UserController.{UserForm, userForm}
 import models.Tables._
+import play.api.data.Forms._
+import play.api.data._
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.mvc._
-import slick.jdbc.JdbcProfile
 import slick.jdbc.H2Profile.api._
+import slick.jdbc.JdbcProfile
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 // ・Playではコントローラはクラスとして実装します
 // ・@InjectはDIのためのアノテーションです
@@ -19,18 +22,31 @@ class UserController @Inject()(implicit ec: ExecutionContext, val dbConfigProvid
   /**
    * 一覧表示
    */
+  // IDの昇順にすべてのユーザ情報を取得
+  // 一覧画面を表示
   def list: Action[AnyContent] = Action.async { implicit rs =>
-    // IDの昇順にすべてのユーザ情報を取得
-    db.run(Users.sortBy(t => t.id).result).map { users =>
-      // 一覧画面を表示
-      Ok(views.html.users.list(users))
-    }
+    db.run(Users.sortBy(t => t.id).result)
+      .map { users => Ok(views.html.users.list(users)) }
   }
 
   /**
    * 編集画面表示
    */
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]): Action[AnyContent] = Action.async { implicit rs =>
+    // リクエストパラメータにIDが存在する場合
+    val form = if (id.isDefined) {
+      // IDからユーザ情報を1件取得
+      // 値をフォームに詰める
+      db.run(Users.filter(t => t.id === id.get.bind).result.head)
+        .map { user => userForm.fill(UserForm(Some(user.id), user.name, user.companyId)) }
+    } else Future(userForm) // リクエストパラメータにIDが存在しない場合
+
+    // 会社一覧を取得
+    form.flatMap { form =>
+      db.run(Companies.sortBy(_.id).result)
+        .map { companies => Ok(views.html.users.edit(form, companies)) }
+    }
+  }
 
   /**
    * 登録実行
@@ -47,4 +63,18 @@ class UserController @Inject()(implicit ec: ExecutionContext, val dbConfigProvid
    */
   def remove(id: Long) = TODO
 
+}
+
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id" -> optional(longNumber),
+      "name" -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
 }
