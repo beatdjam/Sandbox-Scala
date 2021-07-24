d01ced54f74e585342e50711dc6e7cf59a954a10
T.Hori <beat3526@gmail.com>
2021-07-24 23:42:20 +0900

    PlayFramework Controllerの記述を修正 https://www.playframework.com/documentation/2.8.x/PlaySlick#DatabaseConfig-via-runtime-dependency-injection

--- play-scala-seed/app/controllers/JsonController.scala
+++ play-scala-seed/app/controllers/JsonController.scala
@@ -1,12 +1,15 @@
 package controllers
 
 import play.api.db.slick._
-import play.api.mvc.ControllerHelpers
+import play.api.mvc.{AbstractController, ControllerComponents}
 import slick.jdbc.JdbcProfile
 
 import javax.inject.Inject
+import scala.concurrent.ExecutionContext
 
-class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends ControllerHelpers
+class JsonController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(
+  implicit ec: ExecutionContext
+) extends AbstractController(cc)
   with HasDatabaseConfigProvider[JdbcProfile] {
 
   /**
