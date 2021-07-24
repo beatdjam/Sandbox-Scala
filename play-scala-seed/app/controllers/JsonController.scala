9253e5f81fe456644031b47312d6bc116a074c94
T.Hori <beat3526@gmail.com>
2021-07-25 00:21:56 +0900

    PlayFramework ユーザ一覧APIの実装

--- play-scala-seed/app/controllers/JsonController.scala
+++ play-scala-seed/app/controllers/JsonController.scala
@@ -1,21 +1,30 @@
 package controllers
 
+import controllers.JsonController._
+import models.Tables
+import models.Tables._
 import play.api.db.slick._
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.libs.functional.syntax.{toFunctionalBuilderOps, unlift}
+import play.api.libs.json.{Json, OWrites, __}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import slick.jdbc.H2Profile.api._
 import slick.jdbc.JdbcProfile
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
-class JsonController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(
-  implicit ec: ExecutionContext
-) extends AbstractController(cc)
-  with HasDatabaseConfigProvider[JdbcProfile] {
+
+class JsonController @Inject()(implicit ec: ExecutionContext, val dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)
+  extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
   /**
    * 一覧表示
    */
-  def list = TODO
+  def list: Action[AnyContent] = Action.async { implicit rs =>
+    // IDの昇順にすべてのユーザ情報を取得
+    db.run(Users.sortBy(t => t.id).result)
+      .map { users => Ok(Json.obj("users" -> users)) } // ユーザの一覧をJSONで返す
+  }
 
   /**
    * ユーザ登録
@@ -31,4 +40,13 @@ class JsonController @Inject()(protected val dbConfigProvider: DatabaseConfigPro
    * ユーザ削除
    */
   def remove(id: Long) = TODO
+}
+
+object JsonController {
+  // UsersRowをJSONに変換するためのWritesを定義
+  implicit val usersRowWritesWrites: OWrites[Tables.UsersRow] = (
+    (__ \ "id").write[Long] and
+      (__ \ "name").write[String] and
+      (__ \ "companyId").writeNullable[Int]
+    ) (unlift(UsersRow.unapply))
 }
