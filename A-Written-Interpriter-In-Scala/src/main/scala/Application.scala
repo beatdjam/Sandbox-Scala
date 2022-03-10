59d33976596d3f54d8ec87a266cf2a5490ed76ed
T.Hori <beat3526@gmail.com>
2022-03-11 01:50:05 +0900

    A Writtern Interpriter In GoをScalaで書く REPLの実装

--- A-Written-Interpriter-In-Scala/src/main/scala/Application.scala
+++ A-Written-Interpriter-In-Scala/src/main/scala/Application.scala
@@ -1,5 +1,25 @@
+import lexer.Lexer
+import token.EOF
+
+import scala.io.StdIn.readLine
+import scala.util.control.Breaks.{break, breakable}
+
 object Application {
   def main(args: Array[String]): Unit = {
-    println(s"hello ${args.mkString}")
+    val prompt = ">>"
+    println("This is the Monkey programming language!")
+    while (true) {
+      val input = readLine(prompt)
+      if (input == "") return
+
+      val lexer = Lexer.from(input)
+      breakable {
+        while (true) {
+          val tok = lexer.nextToken()
+          if (tok.tokenType == EOF) break
+          println(tok)
+        }
+      }
+    }
   }
 }
